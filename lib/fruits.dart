4c857b3048455a964ff66e372c1e6c6887e64a97
Hichem <benabadji.hichem@gmail.com>
2023-08-11 14:42:37 +0100

    final edit

---
+++ lib/fruits.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/pic.dart';
+
+class MyFruits extends StatelessWidget {
+  const MyFruits({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          backgroundColor: Colors.orange,
+          title: const Text(
+            'Fruits',
+            style: TextStyle(
+                fontSize: 25, fontFamily: 'Pacifico', color: Colors.white),
+          ),
+          centerTitle: true,
+        ),
+        body: const MyPic(),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pic.dart';
+import 'package:flutter_application_1/fruits.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.white,
-        appBar: AppBar(
-          backgroundColor: Colors.orange,
-          title: const Text(
-            'Fruits',
-            style: TextStyle(
-                fontSize: 25, fontFamily: 'Pacifico', color: Colors.white),
-          ),
-          centerTitle: true,
-        ),
-        body: const MyPic(),
-      ),
-    ),
+    const MyFruits(),
   );
 }
